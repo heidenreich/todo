d3def3e2e6ea0c8b43f5168edf2658da3bfadb40
Todd Heidenreich <jtheidenreich@gmail.com>
2014-02-11 08:04:18 -0500

    testing in place

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,80 @@
-console.log('\'Allo \'Allo!');
+console.log('Here goes nothing....');
+
+
+
+
+
+
+
+var todoList = [
+	{
+   	description: 'Need bread',
+   	done: false,
+   	id: _.uniqueId()
+  	},
+  	{
+   	description: 'kill bill',
+   	done: true,
+   	id: _.uniqueId()
+  },
+  {
+   	description: 'fly a kite',
+   	done: false,
+   	id: _.uniqueId()
+  	}
+]
+
+
+$(document).ready(function(){
+
+  	var todoTemplate = _.template($('.todo-template').text())
+
+  // render preloaded data
+  	_.each(todoList, function(item){
+  		$('.todo-items').prepend( todoTemplate(item) )
+  	})
+
+  	$('.todo-items').on('click', '.js-remove-todo', function(){
+    	var parentId = $(this).parent().attr('id').split('-')[1];
+
+    	todoList = _.reject(todoList, function(item){ 
+      return item.id == parentId;
+   	})
+
+    	$(this).parent().remove();
+
+  	})
+
+  // Setup Add Button click event
+  	$('.js-add-todo').click(function(){
+
+    	var description = $('.js-new-todo-input').val();
+
+    	var todo = {
+      	description: description,
+      	done: false,
+      	id: _.uniqueId()
+    	}
+
+    	var renderedTemplate = todoTemplate(todo);
+
+    	$('.todo-items').prepend(renderedTemplate);
+  	
+
+  	})
+
+  	$('.todo-items').on('click', '.js-complete-todo', function(){
+  		
+  		$(this).siblings('.description').toggleClass('completed');
+
+  		$(this).parent(_.each(todoList, function(item){
+    			item.done = true;
+ 		 }))
+
+  	})
+
+});
+
+
+
+
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,40 @@
 /* global describe, it */
 
 (function () {
-    'use strict';
+   'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+   describe('In our Todo App', function () {
+      describe('when the done button is clicked', function () {
+         it('should add a new todo div when the input it populated', function () {
 
+              // Add text to the .new-todo input
+         	$('.js-new-todo-input').val('Drink beer');
+
+              // Click the "add" button
+          	$('.js-add-todo').click();
+
+              // Store the value of the first todo div's text
+           	var firstTodoText = $('.todo-item').first().children('.description').text();
+
+
+         	expect(firstTodoText).to.contain('Drink beer');
+
+         });
+      });
+
+       describe('when the delete button is clicked', function () {
+            it('should reduce the length of the array by 1', function () {
+            	var todoList = [{description: 'Do the Dishes', done: false, id: _.uniqueId()}, {description: 'Read a Book', done: true, id: _.uniqueId()}, {description: 'Work Out', done: false, id: _.uniqueId()}]				
+            	todoList.remove([0])
+              	expect(todoList.length).to.equal(2);
             });
+
+            it("should return true on successful removal", function(){
+        	  	var todoList = [{description: 'Do the Dishes', done: false, id: _.uniqueId()}, {description: 'Read a Book', done: true, id: _.uniqueId()}, {description: 'Work Out', done: false, id: _.uniqueId()}]				
+            	todoList.remove([0])
+            	expect(todoList.remove([0])).to.equal(true);
+        	});
         });
-    });
+   });
+
 })();
